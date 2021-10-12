fb7162c383a920e8b2337621717dc959bc21bd70
ronalddsilva <ronald@rbmweb.com.br>
2021-10-12 09:30:06 -0300

    teste

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 type Props = {
   title: string
 }
+const bla = 'var'
 
 export default function Home({ title = 'React' }: Props) {
-  const bla = 'var'
   return (
     <>
       <div>{title}</div>
